1dba1d819a5c16a92e02b502951638e9d1145ae6
SS1GGzxc <Sterletivakiril@gmail.com>
2025-02-09 18:35:30 +0400

    update upstream(0.3.0)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 export const Hex = {
-  encrypt: async function(str: string) {
-    var hex = '';
-    for(var i=0;i<str.length;i++) {
+  encrypt: function(str: string) {
+    let hex = "";
+    for(let i=0;i<str.length;i++) {
       hex += '' + str.charCodeAt(i).toString(16);
     }
     return hex;
   },
-  decrypt: async function(hex: string) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2){
-      str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  decrypt: function(hex: string) {
+    let str = "";
+    for (let i = 0; i < hex.length; i += 2){
+      str += String.fromCharCode(parseInt(hex.substring(i, 2), 16));
     }
     return str;
   }
@@ -18,13 +18,12 @@ export const Base64 = {
   _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
   encode : function (input: string) {
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
+    let output = "";
+    let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
 
     input = _utf8_encode(input);
 
-    while (i < input.length) {
+    for(let i = 0; i < input.length;) {
       chr1 = input.charCodeAt(i++);
       chr2 = input.charCodeAt(i++);
       chr3 = input.charCodeAt(i++);
@@ -40,21 +39,18 @@ export const Base64 = {
         enc4 = 64;
       }
 
-      output = output +
-      Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
-      Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
+      output = output + Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) + Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
     }
     return output;
   },
   decode : function (input: string) {
-    var output = "";
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
+    let output = "";
+    let chr1, chr2, chr3;
+    let enc1, enc2, enc3, enc4;
 
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    input = input.replace(/[^A-Za-z0-9+\/=]/g, "");
 
-    while (i < input.length) {
+    for(let i = 0; i < input.length;) {
       enc1 = Base64._keyStr.indexOf(input.charAt(i++));
       enc2 = Base64._keyStr.indexOf(input.charAt(i++));
       enc3 = Base64._keyStr.indexOf(input.charAt(i++));
@@ -77,34 +73,33 @@ export const Base64 = {
     return output;
   }
 }
+
 function _utf8_encode(string: string) {
   string = string.replace(/\r\n/g,"\n");
-  var utftext = "";
+  let utftext = "";
 
-  for (var n = 0; n < string.length; n++) {
-    var c = string.charCodeAt(n);
+  for (let n = 0; n < string.length; n++) {
+    let c = string.charCodeAt(n);
 
     if (c < 128) {
       utftext += String.fromCharCode(c);
-    }
-    else if((c > 127) && (c < 2048)) {
+    } else if((c > 127) && (c < 2048)) {
       utftext += String.fromCharCode((c >> 6) | 192);
       utftext += String.fromCharCode((c & 63) | 128);
-    }
-    else {
+    } else {
       utftext += String.fromCharCode((c >> 12) | 224);
       utftext += String.fromCharCode(((c >> 6) & 63) | 128);
       utftext += String.fromCharCode((c & 63) | 128);
     }
+  }
+  return utftext;
 }
-return utftext;
-}
-function _utf8_decode(utftext: string) {
-  var string = "";
-  var i = 0;
-  var c = 0, c3 = 0, c2 = 0;
 
-  while ( i < utftext.length ) {
+function _utf8_decode(utftext: string) {
+  let string = "";
+  let c = 0, c3 = 0, c2 = 0;
+
+  for(let i = 0; i < utftext.length;) {
     c = utftext.charCodeAt(i);
 
     if (c < 128) {
